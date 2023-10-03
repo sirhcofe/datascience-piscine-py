690dfaf5387b617c0ad50749cbec16ee637eaebd
Zer <livefatezz@gmail.com>
2023-10-03 20:25:37 +0800

    module_2: done ex01

--- 3-oop/ex01/S1E7.py
+++ 3-oop/ex01/S1E7.py
@@ -5,10 +5,6 @@ class Baratheon(Character):
     """
     Class representing the Baratheon family
     """
-    # Set attributes
-    family_name = "Baratheon"
-    eyes = "brown"
-    hairs = "dark"
 
     def __init__(self, first_name, is_alive=True):
         """
@@ -23,31 +19,41 @@ class Baratheon(Character):
             Whether the character is alive (default to True)
         """
         super().__init__(first_name, is_alive)
+        # Set attributes
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
 
     def __str__(self):
-        return f"{self.family_name}, {self.eyes}, {self.hairs}"
+        return f"Vector: ({self.family_name}, {self.eyes}, {self.hairs})"
 
     def __repr__(self):
-        return f"{self.family_name}, {self.eyes}, {self.hairs}"
+        return f"Vector: ({self.family_name}, {self.eyes}, {self.hairs})"
+
+    def die(self):
+        super().die()
 
 
 class Lannister(Character):
     """
     Class representing the Lannister family
     """
-    # Set attributes
-    family_name = "Lannister"
-    eyes = "blue"
-    hairs = "light"
 
     def __init__(self, first_name, is_alive=True):
         super().__init__(first_name, is_alive)
+        # Set attributes
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
 
     def __str__(self):
-        return f"{self.family_name}, {self.eyes}, {self.hairs}"
+        return f"Vector: ({self.family_name}, {self.eyes}, {self.hairs})"
 
     def __repr__(self):
-        return f"{self.family_name}, {self.eyes}, {self.hairs}"
+        return f"Vector: ({self.family_name}, {self.eyes}, {self.hairs})"
+
+    def die(self):
+        super().die()
 
     @classmethod
     def create_lannister(cls, first_name, is_alive=True):
